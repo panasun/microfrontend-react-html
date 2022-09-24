3594182747917207d8ae1b53d8bfc43e4d09c856
Panasun Sunanta <panasunsunanta@gmail.com>
2022-09-24 23:32:56 +0700

    add fullscreen div

--- child-app/src/App.js
+++ child-app/src/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 const App = (props) => {
   const [count, setCount] = useState(0);
   console.log(props);
+
+  const toggleFullScreen = () => {
+    const elem = document.getElementById("root");
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  };
+
   return (
-    <div className="App" style={{
-      backgroundColor: "green"
-    }}>
+    <div
+      className="App"
+      style={{
+        backgroundColor: "green",
+        height: "400px",
+      }}
+    >
       <header className="App-header">
         <p style={{ color: "red" }}>
           Edit <code>src/App.js</code> and save to reload.
@@ -25,9 +39,10 @@ const App = (props) => {
         <p>window.location.href {window.location.href}</p>
         <p>Count: {count}</p>
         <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => toggleFullScreen()}>Toggle Fullscreen</button>
       </header>
-    </div >
+    </div>
   );
-}
+};
 
 export default App;
